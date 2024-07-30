974abc6bb2bb1a72042f4ba2eff36551546a6994
rezahusain <rezahusain99@gmail.com>
2024-07-30 13:12:16 -0500

    Added comments

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#home", as: 'landing_page'
 
+  get "comments/:id/new" => "comments#new", as: :new_comment_on_request
+
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
 
   # get "/your_first_screen" => "pages#first"
