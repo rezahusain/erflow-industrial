5d3b2072d51e91961340c45341e826806e7c0b98
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2024-08-23 11:27:16 -0500

    Write initial code review

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  #Comment: Great job actually rescuing the error.
 
   # Reroutes after sign ins and sign ups to the right spot
   def after_sign_in_path_for(_resource)
